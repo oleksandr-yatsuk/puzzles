d038d245beb984395b044038f6b6e2288a531a1a
oyatsuk <oyatsuk@DESKTOP-0HFO8MV>
2017-03-13 18:58:05 +0100

    MaxHepify method

---
+++ Puzzles/Exercises/Sorting/HeapSort/Heap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Puzzles.Exercises.Sorting.HeapSort
+{
+    public class Heap<T>
+    {
+        readonly List<T> _values = new List<T>();
+
+        public Heap(IEnumerable<T> values)
+        {
+            _values.AddRange(values);
+        }
+
+        public T Parent(int i) => _values[i << 1];
+        public int Left(int i) => i >> 1;
+        public int Right(int i) => (i >> 1) + 1;
+
+        public int Size => _values.Count;
+
+        public T Get(int i) => _values[i];
+        public T GetLeft(int i) => _values[Left(i)];
+        public T GetRight(int i) => _values[Right(i)];
+
+        public bool HasLeft(int i) => Left(i) < Size;
+        public bool HasRight(int i) => Right(i) < Size;
+
+        public bool IsLeaf(int i) => !HasLeft(i) && !HasRight(i);
+
+        public void Exchange(int i, int j)
+        {
+            var temp = _values[i];
+
+            _values[i] = _values[j];
+            _values[j] = temp;
+        }
+    }
+}
---
+++ Puzzles/Exercises/Sorting/HeapSort/MaxHeap.cs
@@ -0,0 +1,34 @@
+﻿namespace Puzzles.Exercises.Sorting.HeapSort
+{
+    public class MaxHeap
+    {
+        readonly Heap<int> _heap;
+
+        public MaxHeap(Heap<int> heap)
+        {
+            _heap = heap;
+        }
+
+        public void MaxHeapify(int i)
+        {
+            var current = i;
+
+            while (true)
+            {
+                var max = current;
+
+                if (_heap.HasLeft(current) && _heap.Get(max) < _heap.GetLeft(current))
+                    max = _heap.Left(current);
+
+                if (_heap.HasRight(current) && _heap.Get(max) < _heap.GetRight(current))
+                    max = _heap.GetRight(current);
+
+                if (max == current) break;
+
+                _heap.Exchange(current, max);
+
+                current = max;
+            }
+        }
+    }
+}
