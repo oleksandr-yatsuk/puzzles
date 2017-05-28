10faf99edfb1d219205298b2b25097c4c9cb6fd7
Oleksandr Yatsuk <oleksandr.yatsuk@gmail.com>
2017-05-28 21:16:43 +0200

    Counting sort

---
+++ Puzzles.Tests/Sorting/CountingSort/SortedArrayTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Puzzles.Exercises.Sorting.CountingSort;
+using Xunit;
+
+namespace Puzzles.Tests.Sorting.CountingSort
+{
+    public class SortedArrayTests
+    {
+        [Theory]
+        [InlineData(new [] {1, 2, 3, 4, 5, 1}, 5)]
+        [InlineData(new [] {1, 1, 9, 7}, 9)]
+        [InlineData(new [] {10, 5, 4, 3}, 15)]
+        [InlineData(new [] {1, 2, 2, 2, 2, 1, 1, 2, 1}, 2)]
+        [InlineData(new [] {77, 55, 11, 4, 74, 99, 0, 12, 11, 9}, 100)]
+        public void ValuesAreSortedInAscendingOrder(int[] values, int max)
+        {
+            // SUT
+            var array = new SortedArray(values, max);
+
+            // ACT
+            var actual = array.SortedValues;
+
+            // ASSERT
+            actual.Should().BeInAscendingOrder();
+        }
+
+        [Theory]
+        [InlineData(new [] {1, 2, 3, 4, 5, 1}, 5)]
+        [InlineData(new [] {1, 1, 9, 7}, 9)]
+        [InlineData(new [] {10, 5, 4, 3}, 15)]
+        [InlineData(new [] {1, 2, 2, 2, 2, 1, 1, 2, 1}, 2)]
+        [InlineData(new [] {77, 55, 11, 4, 74, 99, 0, 12, 11, 9}, 100)]
+        public void ValuesAreSortedInDescendingOrder(int[] values, int max)
+        {
+            // SUT
+            var array = new SortedArray(values, max);
+
+            // ACT
+            var actual = array.DescendingSortedValues;
+
+            // ASSERT
+            actual.Should().BeInDescendingOrder();
+        }
+    }
+}
---
+++ Puzzles/Exercises/Sorting/CountingSort/SortedArray.cs
@@ -0,0 +1,75 @@
+﻿namespace Puzzles.Exercises.Sorting.CountingSort
+{
+    public class SortedArray
+    {
+        readonly int[] values;
+        readonly int max;
+
+        public SortedArray(int[] values, int max)
+        {
+            this.values = values;
+            this.max = max;
+        }
+
+        public int[] SortedValues => SortValues();
+        public int[] DescendingSortedValues => SortValuesInDescendingOrder();
+
+        int[] SortValuesInDescendingOrder()
+        {
+            var counters = GetCounters();
+
+            for (var i = 1; i < max + 1; i++)
+            {
+                counters[i] += counters[i - 1];
+            }
+
+            var sortedValues = new int[values.Length];
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                var currentValue = values[i];
+
+                sortedValues[values.Length - counters[currentValue]] = currentValue;
+
+                counters[currentValue]--;
+            }
+
+            return sortedValues;
+        }
+
+        int[] SortValues()
+        {
+            var counters = GetCounters();
+
+            for (var i = 1; i < max + 1; i++)
+            {
+                counters[i] += counters[i - 1];
+            }
+
+            var sortedValues = new int[values.Length];
+            
+            for (var i = values.Length - 1; i >= 0; i--)
+            {
+                var currentValue = values[i];
+                var counter = --counters[currentValue];
+
+                sortedValues[counter] = currentValue;
+                
+            }
+
+            return sortedValues;
+        }
+
+        int[] GetCounters()
+        {
+            var counters = new int[max + 1];
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                counters[values[i]]++;
+            }
+
+            return counters;
+        }
+    }
+}
